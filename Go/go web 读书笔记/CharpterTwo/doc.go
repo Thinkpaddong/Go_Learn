1dc362ee333ec9fa993ff0dd5cd1788f6bfaa7ea
Thinkpaddong <zying_dong@163.com>
2021-12-11 20:13:44 +0800

    20211211

--- Go/go web 读书笔记/CharpterTwo/doc.go
+++ Go/go web 读书笔记/CharpterTwo/doc.go
@@ -1,7 +1,8 @@
 /*
  * @Author: Thinkpaddong
  * @Date: 2021-12-11 16:40:25
- * @LastEditTime: 2021-12-11 16:40:26
+ * @LastEditTime: 2021-12-11 16:42:33
  * @Description:
  * @FilePath: /Test-for-github/Go/go web 读书笔记/第2章/doc.go
  */
+package main
